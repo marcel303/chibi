92be85221056208c3ed2cd10ac5d498bad4520e2
Marcel Smit <marcel303@gmail.com>
2018-12-04 10:47:50 +0100

    Fix for how distribution flags were set. Cmake concatenated them using ';' instead of seeing separate defines.

--- main.cpp
+++ main.cpp
@@ -2292,8 +2292,8 @@ struct CMakeWriter
 				sb.Append("\n");
 				
 				sb.Append("list(APPEND CMAKE_CONFIGURATION_TYPES Distribution)\n");
-				sb.Append("set(CMAKE_CXX_FLAGS_DISTRIBUTION ${CMAKE_CXX_FLAGS_RELEASE} -DCHIBI_BUILD_DISTRIBUTION=1)\n");
-				sb.Append("set(CMAKE_C_FLAGS_DISTRIBUTION ${CMAKE_C_FLAGS_RELEASE} -DCHIBI_BUILD_DISTRIBUTION=1)\n");
+				sb.Append("set(CMAKE_CXX_FLAGS_DISTRIBUTION \"${CMAKE_CXX_FLAGS_RELEASE} -DCHIBI_BUILD_DISTRIBUTION=1\")\n");
+				sb.Append("set(CMAKE_C_FLAGS_DISTRIBUTION \"${CMAKE_C_FLAGS_RELEASE} -DCHIBI_BUILD_DISTRIBUTION=1\")\n");
 				sb.Append("\n");
 
 			// todo : global compile options should be user-defined
