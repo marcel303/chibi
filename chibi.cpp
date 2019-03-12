d7f7589e054215f3593add98498617fb99da3c7c
Marcel Smit <marcel303@gmail.com>
2019-03-12 21:48:44 +0100

    - Moving various string functions to their own file to create some overview. - Added a few comments about unsafe globals (when the chibi library will be used concurrently).

--- chibi.cpp
+++ chibi.cpp
@@ -1,4 +1,5 @@
 #include "filesystem.h"
+#include "stringhelpers.h"
 
 #include <algorithm>
 #include <deque>
@@ -73,7 +74,7 @@ brew install ccache
 
 #define STRING_BUFFER_SIZE (1 << 12)
 
-static ssize_t s_current_line_length = 0;
+static ssize_t s_current_line_length = 0; // fixme : make safe for concurrent use of library version of chibi
 
 static bool file_exist(const char * path)
 {
@@ -90,131 +91,7 @@ static bool file_exist(const char * path)
 	}
 }
 
-static bool is_whitespace(const char c)
-{
-	return isspace(c);
-}
-
-static bool is_comment_or_whitespace(const char * line)
-{
-	for (int i = 0; line[i] != 0; ++i)
-	{
-		if (line[i] == '#')
-			return true;
-		
-		if (is_whitespace(line[i]) == false)
-			return false;
-	}
-	
-	return true;
-}
-
-// eats an arbitrary word and stores the result in 'word'. has built-in support for quoted strings
-static bool eat_word_v2(char *& line, const char *& word)
-{
-	while (*line != 0 && is_whitespace(*line) == true)
-		line++;
-	
-	if (*line == 0)
-		return false;
-	
-	const bool isQuoted = *line == '"';
-
-	if (isQuoted)
-	{
-		line++;
-
-		word = line;
-		
-		while (*line != 0 && *line != '"')
-			line++;
-	}
-	else
-	{
-		word = line;
-		
-		while (*line != 0 && is_whitespace(*line) == false)
-			line++;
-	}
-	
-	if (line > word)
-	{
-		if (*line != 0)
-		{
-			*line = 0;
-			line++;
-		}
-		
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-}
-
-// checks if 'line' begin with 'word' and eats 'word' from 'line' when it does
-static bool eat_word(char * & line, const char * word)
-{
-	while (is_whitespace(*line))
-		line++;
-	
-	int index = 0;
-	
-	while (word[index] != 0)
-	{
-		if (line[index] != word[index])
-			return false;
-		
-		index++;
-	}
-
-	if (is_whitespace(line[index]) == false && line[index] != 0)
-		return false;
-
-	while (is_whitespace(line[index]))
-			index++;
-
-	line += index;
-	return true;
-}
-
-static bool do_concat(char *& dst, int & dstSize, const char * s)
-{
-	for (int i = 0; s[i] != 0; ++i)
-	{
-		if (dstSize == 0)
-			return false;
-		
-		*dst = s[i];
-		
-		dst += 1;
-		dstSize -= 1;
-	}
-	
-	if (dstSize == 0)
-		return false;
-	
-	*dst = 0;
-	
-	return true;
-}
-
-/*public*/ bool concat(char * dst, int dstSize, const char * s1, const char * s2 = nullptr, const char * s3 = nullptr, const char * s4 = nullptr)
-{
-	return
-		do_concat(dst, dstSize, s1) &&
-		(s2 == nullptr || do_concat(dst, dstSize, s2)) &&
-		(s3 == nullptr || do_concat(dst, dstSize, s3)) &&
-		(s4 == nullptr || do_concat(dst, dstSize, s4));
-}
-
-static bool copy_string(char * dst, int dstSize, const char * s)
-{
-	return concat(dst, dstSize, s);
-}
-
-static std::vector<std::string> s_currentFile;
+static std::vector<std::string> s_currentFile; // fixme : make safe for concurrent use of library version of chibi
 
 struct ChibiFileScope
 {
@@ -267,122 +144,6 @@ static void report_error(const char * line, const char * format, ...)
 	printf("error: %s\n", text);
 	if (!s_currentFile.empty())
 		printf("in file: %s\n", s_currentFile.back().c_str());
-}
-
-static bool get_path_from_filename(const char * filename, char * path, int pathSize)
-{
-	if (!concat(path, pathSize, filename))
-		return false;
-
-	char * term = nullptr;
-
-	for (int i = 0; path[i] != 0; ++i)
-		if (path[i] == '/')
-			term = &path[i];
-
-	if (term == nullptr)
-	{
-		report_error(nullptr, "invalid path: %s", filename);
-		return false;
-	}
-
-	*term = 0;
-	
-	return true;
-}
-
-static std::string get_path_extension(const std::string & path, const bool to_lower)
-{
-	size_t pos = path.find_last_of('.');
-	
-	if (pos == std::string::npos)
-		return std::string();
-	else
-		pos++;
-	
-	std::string extension = path.substr(pos);
-	
-	if (to_lower)
-	{
-		for (auto & c : extension)
-			c = tolower(c);
-	}
-	
-	return extension;
-}
-
-static bool string_starts_with(const std::string & text, const std::string & substring)
-{
-	const size_t length1 = text.length();
-	const size_t length2 = substring.length();
-
-	if (length1 < length2)
-		return false;
-
-	for (size_t i = 0; i < length2; ++i)
-		if (text[i] != substring[i])
-			return false;
-
-	return true;
-}
-
-static bool string_ends_with(const std::string & text, const std::string & substring)
-{
-	const size_t length1 = text.length();
-	const size_t length2 = substring.length();
-
-	if (length1 < length2)
-		return false;
-
-	for (size_t i = length1 - length2, j = 0; i < length1; ++i, ++j)
-		if (text[i] != substring[j])
-			return false;
-
-	return true;
-}
-
-static bool match_wildcard(const char * in_text, const char * wildcard)
-{
-	const char * text = in_text;
-	
-	while (wildcard[0] != 0)
-	{
-		if (wildcard[0] == ';')
-		{
-			text = in_text;
-			wildcard++;
-		}
-		else if (wildcard[0] == '*')
-		{
-			if (wildcard[1] == 0 || wildcard[1] == ';')
-				return true;
-			else
-			{
-				while (text[0] != 0 && text[0] != wildcard[1])
-					text++;
-				
-				if (text[0] == 0)
-					return false;
-				
-				wildcard++;
-			}
-		}
-		else
-		{
-			if (text[0] != wildcard[0])
-			{
-				while (wildcard[0] != 0 && wildcard[0] != ';')
-					wildcard++;
-			}
-			else
-			{
-				text++;
-				wildcard++;
-			}
-		}
-	}
-	
-	return text[0] == 0;
 }
 
 static bool is_absolute_path(const char * path)
@@ -560,6 +321,8 @@ struct ChibiInfo
 		}
 	}
 };
+
+// fixme : make all of these safe for concurrent use of library version of chibi
 
 static ChibiInfo s_chibiInfo;
 
@@ -744,7 +507,7 @@ static bool process_chibi_file(const char * filename)
 
 	if (!get_path_from_filename(filename, chibi_path, PATH_MAX))
 	{
-		report_error(nullptr, "failed to get path from chibi filename");
+		report_error(nullptr, "failed to get path from chibi filename: %s", filename);
 		return false;
 	}
 	
---
+++ stringhelpers.h
@@ -0,0 +1,238 @@
+#pragma once
+
+static bool is_whitespace(const char c)
+{
+	return isspace(c);
+}
+
+static bool is_comment_or_whitespace(const char * line)
+{
+	for (int i = 0; line[i] != 0; ++i)
+	{
+		if (line[i] == '#')
+			return true;
+		
+		if (is_whitespace(line[i]) == false)
+			return false;
+	}
+	
+	return true;
+}
+
+// eats an arbitrary word and stores the result in 'word'. has built-in support for quoted strings
+static bool eat_word_v2(char *& line, const char *& word)
+{
+	while (*line != 0 && is_whitespace(*line) == true)
+		line++;
+	
+	if (*line == 0)
+		return false;
+	
+	const bool isQuoted = *line == '"';
+
+	if (isQuoted)
+	{
+		line++;
+
+		word = line;
+		
+		while (*line != 0 && *line != '"')
+			line++;
+	}
+	else
+	{
+		word = line;
+		
+		while (*line != 0 && is_whitespace(*line) == false)
+			line++;
+	}
+	
+	if (line > word)
+	{
+		if (*line != 0)
+		{
+			*line = 0;
+			line++;
+		}
+		
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+// checks if 'line' begin with 'word' and eats 'word' from 'line' when it does
+static bool eat_word(char * & line, const char * word)
+{
+	while (is_whitespace(*line))
+		line++;
+	
+	int index = 0;
+	
+	while (word[index] != 0)
+	{
+		if (line[index] != word[index])
+			return false;
+		
+		index++;
+	}
+
+	if (is_whitespace(line[index]) == false && line[index] != 0)
+		return false;
+
+	while (is_whitespace(line[index]))
+			index++;
+
+	line += index;
+	return true;
+}
+
+static bool do_concat(char *& dst, int & dstSize, const char * s)
+{
+	for (int i = 0; s[i] != 0; ++i)
+	{
+		if (dstSize == 0)
+			return false;
+		
+		*dst = s[i];
+		
+		dst += 1;
+		dstSize -= 1;
+	}
+	
+	if (dstSize == 0)
+		return false;
+	
+	*dst = 0;
+	
+	return true;
+}
+
+/*public*/ bool concat(char * dst, int dstSize, const char * s1, const char * s2 = nullptr, const char * s3 = nullptr, const char * s4 = nullptr)
+{
+	return
+		do_concat(dst, dstSize, s1) &&
+		(s2 == nullptr || do_concat(dst, dstSize, s2)) &&
+		(s3 == nullptr || do_concat(dst, dstSize, s3)) &&
+		(s4 == nullptr || do_concat(dst, dstSize, s4));
+}
+
+static bool copy_string(char * dst, int dstSize, const char * s)
+{
+	return concat(dst, dstSize, s);
+}
+
+static bool get_path_from_filename(const char * filename, char * path, int pathSize)
+{
+	if (!concat(path, pathSize, filename))
+		return false;
+
+	char * term = nullptr;
+
+	for (int i = 0; path[i] != 0; ++i)
+		if (path[i] == '/')
+			term = &path[i];
+
+	if (term == nullptr)
+		return false;
+
+	*term = 0;
+	
+	return true;
+}
+
+static std::string get_path_extension(const std::string & path, const bool to_lower)
+{
+	size_t pos = path.find_last_of('.');
+	
+	if (pos == std::string::npos)
+		return std::string();
+	else
+		pos++;
+	
+	std::string extension = path.substr(pos);
+	
+	if (to_lower)
+	{
+		for (auto & c : extension)
+			c = tolower(c);
+	}
+	
+	return extension;
+}
+
+static bool string_starts_with(const std::string & text, const std::string & substring)
+{
+	const size_t length1 = text.length();
+	const size_t length2 = substring.length();
+
+	if (length1 < length2)
+		return false;
+
+	for (size_t i = 0; i < length2; ++i)
+		if (text[i] != substring[i])
+			return false;
+
+	return true;
+}
+
+static bool string_ends_with(const std::string & text, const std::string & substring)
+{
+	const size_t length1 = text.length();
+	const size_t length2 = substring.length();
+
+	if (length1 < length2)
+		return false;
+
+	for (size_t i = length1 - length2, j = 0; i < length1; ++i, ++j)
+		if (text[i] != substring[j])
+			return false;
+
+	return true;
+}
+
+static bool match_wildcard(const char * in_text, const char * wildcard)
+{
+	const char * text = in_text;
+	
+	while (wildcard[0] != 0)
+	{
+		if (wildcard[0] == ';')
+		{
+			text = in_text;
+			wildcard++;
+		}
+		else if (wildcard[0] == '*')
+		{
+			if (wildcard[1] == 0 || wildcard[1] == ';')
+				return true;
+			else
+			{
+				while (text[0] != 0 && text[0] != wildcard[1])
+					text++;
+				
+				if (text[0] == 0)
+					return false;
+				
+				wildcard++;
+			}
+		}
+		else
+		{
+			if (text[0] != wildcard[0])
+			{
+				while (wildcard[0] != 0 && wildcard[0] != ';')
+					wildcard++;
+			}
+			else
+			{
+				text++;
+				wildcard++;
+			}
+		}
+	}
+	
+	return text[0] == 0;
+}
